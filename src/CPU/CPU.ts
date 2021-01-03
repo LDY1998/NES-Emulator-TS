55bca857b157f4c09baa49fb97bb9b346759e6b2
Jerry <caliudeyu2017@gmail.com>
2021-01-02 19:22:48 -0800

    basic implementaton of load instruction

--- src/CPU/CPU.ts
+++ src/CPU/CPU.ts
@@ -1,7 +1,13 @@
 
 
 
-
+enum Register {
+    REG_X = "REG_X",
+    REG_Y = "REG_Y",
+    REG_SP = "REG_SP",
+    REG_ACC = "REG_ACC",
+    REG_PC = "REG_PC"
+}
 
 
 /**
@@ -23,7 +29,7 @@ class CPU {
 
     private memory: number[];
 
-    private opt_table: { [optcode: number]: Function};
+    private opt_table: { [optcode: number]: (cycle: number) => number};
 
     private opt_code: number;
 
@@ -31,20 +37,52 @@ class CPU {
     constructor() {
         this.memory = new Array<number> (0x10000);
         this.reset();
+        this.opt_table = {
+            0xA9: this.loadAcc,
+            0xA2: this.loadX
+        };
     }
-
 
     private reset(): void {
         this.REG_ACC = 0;
         this.REG_X = 0;
         this.REG_Y = 0;
         // Reset Stack pointer:
-        this.REG_SP = 0;
+        this.REG_SP = 0x0100;
         // Reset Program counter:
-        this.REG_PC = 0x8000 - 1;
-        this.REG_PC_NEW = 0x8000 - 1;
-        this.memory.fill(0);
+        this.REG_PC = 0xFFFC;
+        this.REG_PC_NEW = 0xFFFC;
         this.resetStatus();
+        this.resetMemory();
+    }
+
+
+    private loadAcc(cycles: number): number {
+        return this.load(cycles, Register.REG_ACC);
+    }
+
+    
+    private loadX(cycles: number): number {
+        return this.load(cycles, Register.REG_X);
+    }
+
+    private load(cycles: number, reg: Register) {
+        const value = this.fetch(cycles);
+        const data = value.data;
+        const register = reg;
+        const exe_cycles = value.cycles;
+
+        this[register] = data;
+        this.status.zero = this[register] === 0 ? 1 : 0;
+        this.status.negative = this[register] >> 7 & 0x01;
+
+        this.REG_PC++;
+
+        return exe_cycles - 1;
+    }
+
+    private resetMemory(): void {
+        this.memory.fill(0);
     }
 
     private resetStatus(): void {
@@ -55,6 +93,27 @@ class CPU {
         this.status["break"] = 0;
         this.status["overflow"] = 0;
         this.status["negative"] = 0;
+    }
+
+
+
+    private execute(cycles: number): void {
+        let exe_cycles = cycles;
+        while (exe_cycles > 0) {
+            const fetched = this.fetch(exe_cycles);
+            const instruction = fetched.data;
+            exe_cycles = fetched.cycles;
+            exe_cycles = this.opt_table[instruction](exe_cycles);
+        }
+    }
+
+    private fetch(cycles: number) {
+        const data = this.memory[this.REG_PC];
+        this.REG_PC++;
+        return  {
+            "data": data,
+            "cycles": cycles-1
+        };
     }
 
 
